bca48f4fc3602a45fa994c9a0a18cdc9a0389378
Kikira234 <37173154+Kikira234@users.noreply.github.com>
2018-09-01 12:52:14 +0200

    Definition of Class

---
+++ data_analysis/visualization_class.py
@@ -0,0 +1,19 @@
+
+# This class will have all function for the visualization part of the GUI for visualization
+
+
+class Visualization():
+
+    def show_column_options(self, data):
+
+        return arrayOfColumnName
+
+    def plot_everything(self, data, columns, type):
+        """
+        data = np.array
+        columns = list[x,y,z]
+        type = e.g. "scatter", "bar"
+        """
+
+
+
