66b4e63514a7b2682c3e63449f6f05cde5a53e5e
Kikira234 <37173154+Kikira234@users.noreply.github.com>
2018-09-15 13:13:59 +0200

    Define functions

--- data_analysis/visualization_class.py
+++ data_analysis/visualization_class.py
@@ -2,23 +2,62 @@
 # This class will have all function for the visualization part of the GUI for visualization
 
 
-class Visualization():
+class Visualization:
 
-    def show_column_options(self, data):
-        """
 
-        :param data:2D (or 3D)  np.array
-        :return: 1D np.array with the possible column names
-        """
-        return arrayOfColumnName
 
     def plot_everything(self, data, columns, type, filter = 0):
-        """
+        """ Everyone
+        Phillip: Bubble Plot
         data = 2D np.array
         columns = list[x,y,z]?
         type = e.g. "scatter", "bar"
         filter = optional to filter the rows
         """
+
+
+class DataParsing:
+
+    def __init__(self, data):
+        self.data = data
+        self.check_data(self.data)
+
+    def check_data(self, data):
+        """ Phillip
+        Check if data is empty.
+        :param data:
+        :return:
+        """
+
+        message = True
+        message = False
+        return message
+
+    def cleanup_data(self, data):
+        """ Francesco
+        Delete Missings and strange values.
+
+        :param data: numpy array
+        :return:
+        """
+
+        return data
+
+    def extract_names(self, data):
+        """ Marie
+        Extract names and the type of the table.
+
+
+        :param data:2D (or 3D)  np.array
+        :return: 1D np.array with the possible column names
+        """
+
+
+        return arrayOfColumnName
+
+
+
+
 
 
 
