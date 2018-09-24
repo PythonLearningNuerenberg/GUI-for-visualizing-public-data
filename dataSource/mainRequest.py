5d9b0b1ff090b59786096c8564610e6b83d9b168
ishansheth <ishansheth@gmail.com>
2018-09-24 21:24:39 +0200

    added config file to list the datasources, file handling

--- dataSource/mainRequest.py
+++ dataSource/mainRequest.py
@@ -4,16 +4,17 @@ import requests
 # TODO: In which format the data will be available
 # TODO: From where the data will come and in which form.
 # TODO: A method to get the data from the below interface
+# TODO: Online/ Offline datasource options
 
 class DataSource:
     '''A singleton Data source class'''
-    '''Since python does not have access specifiers like C++, all the attributes which starts with __ are mangled'''
     __instance = None;
 
     def __init__(self):
         if DataSource.__instance != None:
-            raise Exception("Data Source is defined singleton, can't be used without getInstance()")
+            raise Exception("Singleton class, use getInstance() method to create instances")
         else:
+            self.onlineDataSource = []
             DataSource.__instance = self;
 
     @staticmethod
@@ -23,25 +24,39 @@ class DataSource:
             DataSource();
         return DataSource.__instance
 
-    def getAvailableDataFromGithub(self):
-        page = requests.get("https://github.com/plotly/datasets")
-        # check status here
-        if (page.status_code != 404):
-            # if status code is OK then, think how to get the data from that page
-            print("Status code returned: OK")
-            print(page.status_code)
-            print(page.encoding)
-            print(page.text)
+    def getDataSourceLocations(self):
+        '''read config file to get the data sources'''
+        # with block simplifies file handling, instead of try, except
+        with open("config.txt","r") as configFileHandle:
+            for lines in configFileHandle:
+                # one can check here if the data source link in correctly written here, skip the # comments
+                if(lines[0] != '#'):
+                    self.onlineDataSource.append(lines)
+        if len(self.onlineDataSource) > 0:
+            return True
         else:
-            print("Status code is 404!!")
+            return False
+
+    def generateDataBank(self):
+        '''generates the data bank'''
+        if(self.createDataSource()):
+            for source in self.onlineDataSource:
+                page = requests.get(source)
+                # check status here
+                if (page.status_code != 404):
+                    # if status code is OK then, think how to get the data from that page
+                    print("Status code returned: OK")
+                    print(page.status_code)
+                    print(page.encoding)
+                    print(page.text)
+                else:
+                    print("Status code is 404!!")
 
 
 # check whether the class gives the same instance to all the caller
 
 dataSource1 = DataSource.getInstance();
-dataSource2 = DataSource.getInstance();
-if dataSource1 is dataSource2:
-    print("They are same")
+if(dataSource1.createDataSource()):
+    print("ok")
 else:
-    print("They are not same")
-
+    print("nok")
