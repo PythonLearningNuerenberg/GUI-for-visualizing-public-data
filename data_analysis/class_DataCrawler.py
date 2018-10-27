be60a67684fc1be3839d7c10a1e05ffda9b1e580
Yevgen <>
2018-10-27 17:37:35 +0200

    large improvements on DataCrawler - now working properly

--- data_analysis/class_DataCrawler.py
+++ data_analysis/class_DataCrawler.py
@@ -1,20 +1,21 @@
-from time import sleep
-import random
+import os
 import logging
 import json
-import csv
-import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 class DataCrawler:
-    def __init__(self, filepath):
-        self.filepath = filepath
-
+    def __init__(self, folderpath=''):
         # logging
         logging.basicConfig(level=logging.INFO)
+
+        # config
+        with open('config.json') as config_file:
+            self.config = json.load(config_file)
 
         # selenium
         self.driver_options = Options()
@@ -22,22 +23,68 @@ class DataCrawler:
         self.driver = webdriver.Chrome(self.config['Selenium']['chrome_driver_path'],
                                        chrome_options=self.driver_options)
 
-        # config
-        with open('config.json') as config_file:
-            self.config = json.load(config_file)
+        # target folder
+        self.folderpath = folderpath
+        if self.folderpath is None:
+            self.folderpath = ''
+        if self.folderpath != '' and not os.path.exists(self.folderpath):
+            os.makedirs(self.folderpath)
 
-    def crawl(self, links):
-        for link in links:
-            self.crawl_single_link(link)
+    def crawl_all_csv_files_from_page(self, page_link):
+        # get files list at page
+        csv_file_list = self.crawl_csv_file_list_from_page(page_link)
+        print('Number of files on page: ' + str(len(csv_file_list)))
+        for csv_link in csv_file_list:
+            print('- ' + csv_link['link'])
 
-    def crawl_single_link(self, page_link):
+        print('\n\n')
+
+        # download the content of all files from list
+        for link_on_page in csv_file_list:
+            if '.csv' in link_on_page['text']:
+                print(link_on_page['text'])
+                print('- ' + link_on_page['link'])
+                result = self.download_csv(link_on_page['link'])
+
+                # store
+                print('- storing to: ' + result['filename'])
+                self.store_data_in_file(result['csv'], result['filename'])
+
+                print()
+
+    def crawl_csv_file_list_from_page(self, page_link):
         self.driver.get(page_link)
-        links_on_page = self.driver.find_elements_by_tag_name('a')
-        csv_links_on_page = links_on_page
+        links_elements = self.driver.find_elements_by_tag_name('a')
+        links_on_page = [{'text': link.text, 'link': link.get_attribute('href')}
+                         for link in links_elements if '.csv' in link.text and '.csv' in link.get_attribute('href')]
 
-        for link_on_page in csv_links_on_page:
-            dataframe = pd.read_csv(link_on_page)
-            self.store_data_in_file(dataframe)
+        return links_on_page
 
-    def store_data_in_file(self, data):
-        pass
+    def download_csv(self, link):
+        try:
+            self.driver.get(link)
+            csv_link = self.driver.find_element_by_id('raw-url').get_attribute('href')
+            self.driver.get(csv_link)
+
+            # wait for redirect
+            wait = WebDriverWait(self.driver, 10)
+            wait.until(ec.url_changes(csv_link))
+
+            # get url and data
+            redir_url = self.driver.current_url
+            csv_string = self.driver.find_element_by_tag_name('pre').text
+
+            return {'filename': str(redir_url).split('/')[-1], 'csv': csv_string}
+        except:
+            logging.warning(link)
+
+    def store_data_in_file(self, csv_string, filename):
+        full_path = os.path.join(self.folderpath, filename)
+        with open(full_path, 'w+') as csv_file_handle:
+            csv_file_handle.write(csv_string)
+
+
+if __name__ == '__main__':
+    dc = DataCrawler('Data')
+    github_link = dc.config['github']['csv_page']
+    dc.crawl_all_csv_files_from_page(github_link)
